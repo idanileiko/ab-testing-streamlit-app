1b7ecdaa9ea8a17651d14040c921235812984b96
Irina Danileiko <danileiko.irina@gmail.com>
2025-10-03 13:41:34 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,13 +13,13 @@ st.sidebar.success("Select a page above.")
 st.title("🧪 A/B Testing Statistical Analysis")
 st.write("Upload your experiment data and run statistical tests between groups.")
 st.write("Select a page from the sidebar that fits your stats use-case!")
-st.write("""
-    Binary variables are metrics that only have two options. Examples are:
+st.markdown("""
+    ***Binary variables*** are metrics that only have two options. Examples are:
          - conversion (did users spend or no?)
          - click rates (did users click a link?)
          - retention (did users retain on d7 or no?)
 
-    Continuous variables are metrics that can be any value along a continuous scale. Examples are:
+    ***Continuous variables*** are metrics that can be any value along a continuous scale. Examples are:
         - total amount spent
         - average frame rate
         - total matches played
