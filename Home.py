8915d9fa24ace458c9897a225d20dac26919131d
Irina Danileiko <danileiko.irina@gmail.com>
2025-10-03 13:42:11 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,13 +14,13 @@ st.title("🧪 A/B Testing Statistical Analysis")
 st.write("Upload your experiment data and run statistical tests between groups.")
 st.write("Select a page from the sidebar that fits your stats use-case!")
 st.markdown("""
-    ***Binary variables*** are metrics that only have two options. Examples are:
-         - conversion (did users spend or no?)
-         - click rates (did users click a link?)
-         - retention (did users retain on d7 or no?)
+    **Binary variables** are metrics that only have two options. Examples are:
+    - conversion (did users spend or no?)
+    - click rates (did users click a link?)
+    - retention (did users retain on d7 or no?)
 
-    ***Continuous variables*** are metrics that can be any value along a continuous scale. Examples are:
-        - total amount spent
-        - average frame rate
-        - total matches played
+    **Continuous variables** are metrics that can be any value along a continuous scale. Examples are:
+    - total amount spent
+    - average frame rate
+    - total matches played
 """)
